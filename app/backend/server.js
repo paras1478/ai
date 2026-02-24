335067aa795a19d33d904b8059b0235b73defc6a
paras1478 <pankaj147878@gmail.com>
2026-02-25 00:19:24 +0530

    fix railway host binding

--- app/backend/server.js
+++ app/backend/server.js
@@ -64,4 +64,6 @@ app.use(errorhandler);
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
