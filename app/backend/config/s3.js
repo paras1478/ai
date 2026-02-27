fea5c26dc2364cb2c7a315abfbed556515066f6b
paras1478 <pankaj147878@gmail.com>
2026-02-27 17:57:12 +0530

    fix cors

--- app/backend/config/s3.js
+++ app/backend/config/s3.js
@@ -1,6 +1,6 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import AWS from "aws-sdk";
 
-export const s3 = new S3Client({
+export const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
